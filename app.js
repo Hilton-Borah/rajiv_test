0806395f12d680fe0687953555dee73a8357b794
Hilton-Borah <hiltonborah123@gmail.com>
2024-01-23 00:24:14 +0530

    initial set up

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const corsOptions = {
+  origin: "http://127.0.0.1:5501",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+const connectDB = require("./db/connectDB");
+const postRoutes = require("./routes/postRoutes");
+
+const port = 3000;
+
+app.use(express.json());
+
+app.use("/api/post", postRoutes);
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`Connection is Live at port no. ${port}`);
+});
